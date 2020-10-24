5f38876c73fd9b208a5d80e15de26633bb014f48
Amédée DERA <53271706+a-dera@users.noreply.github.com>
2020-10-24 13:06:57 +0000

    Adding footer

---
+++ Dashboard/footer.php
@@ -0,0 +1,40 @@
+ <div class="app-wrapper-footer">
+                        <div class="app-footer">
+                            <div class="app-footer__inner">
+                                <div class="app-footer-left">
+                                    <ul class="nav">
+                                        <li class="nav-item">
+                                          <span class="nav-link"> Copyright &copy; <?php echo date("Y"); ?> | TresoGest Version 2.6.0
+										  proposé avec  "<i class="pe-7s-like ">
+                                        </i>"   par
+										</span>
+
+                                        </li>
+
+                                        <li class="nav-item">
+                                            <a href="javascript:void(0);" title="TresoGest" class="nav-link">
+                                                Aïda HIE, Hamidou OUEDRAOGO, Amédée DERA 
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <!-- <div class="app-footer-right">
+                                    <ul class="nav">
+                                        <li class="nav-item">
+                                            <a href="javascript:void(0);" title="Ecrivez-moi un mail" class="nav-link">
+                                               @gmail.com
+                                            </a>
+                                        </li> 
+                                        <li class="nav-item">
+                                            <a href="javascript:void(0);" title="Appelez-moi" class="nav-link">
+                                                <div class="badge badge-success mr-2 ml-2">
+                                                    56 86 36 75
+                                                </div>
+
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div> -->
+                            </div>
+                        </div>
+                    </div>
